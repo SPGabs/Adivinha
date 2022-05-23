057d7464cf9b069814bd35e99f5785b22f4ae6e7
SPGabs <105547321+SPGabs@users.noreply.github.com>
2022-05-22 22:40:50 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-var segredo = Math.round(Math.random()*10);
+var segredo = Math.round(Math.random() * 10);
 var input = document.querySelector("input");
 input.focus();
 function verifica() {
 	if(input.value == segredo) {
 		alert("Você Acertou!");
 	} else {
-	alert("Você Errou!");				
+		alert("Você Errou!");				
 	}
 	input.focus();
-	nput.Value = "";
+	input.value = "";
 }
 var button = document.querySelector("button");
 button.onclick = verifica;
